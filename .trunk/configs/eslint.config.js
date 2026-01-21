8f6c3037556665cc6047f1bfb8681bdbac753443
dmezhnov <dmezhnov@gmail.com>
2026-01-21 16:04:21 +0500

    draft-2026-01-21-16-04-21

--- .trunk/configs/eslint.config.js
+++ .trunk/configs/eslint.config.js
@@ -1,10 +1,18 @@
-import js from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js        from '@eslint/js';
+import tsParser  from '@typescript-eslint/parser';
+import tsPlugin  from '@typescript-eslint/eslint-plugin';
 import stylistic from '@stylistic/eslint-plugin';
-// import putout from "eslint-plugin-putout";
+import putout    from 'eslint-plugin-putout';
 
 export default [
+    {
+        plugins: {
+            putout,
+        },
+        rules: {
+            'convert-esm-to-commonjs': 'off'
+        },
+    },
     js.configs.recommended,
     stylistic.configs['disable-legacy'],
     {
@@ -13,7 +21,7 @@ export default [
         },
         rules: {
             '@stylistic/indent': ['error', 4, { SwitchCase: 1 }],
-            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/semi':   ['error', 'always'],
             '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
         },
     },
@@ -21,36 +29,28 @@ export default [
         files: ['**/*.js'],
         languageOptions: {
             globals: {
-                console: 'readonly',
-                process: 'readonly',
-                module: 'readonly',
-                require: 'readonly',
-                __dirname: 'readonly',
+                console:    'readonly',
+                process:    'readonly',
+                module:     'readonly',
+                require:    'readonly',
+                __dirname:  'readonly',
                 __filename: 'readonly',
             },
         },
     },
-    // {
-    //     plugins: {
-    //         putout,
-    //     },
-    //     rules: {
-    //         "putout/putout": "error",
-    //     },
-    // },
     {
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
             parser: tsParser,
             parserOptions: {
                 ecmaVersion: 'latest',
-                sourceType: 'module',
+                sourceType:  'module',
             },
             globals: {
                 console: 'readonly',
                 process: 'readonly',
-                Bun: 'readonly',
-                module: 'readonly',
+                Bun:     'readonly',
+                module:  'readonly',
                 require: 'readonly',
             },
         },
